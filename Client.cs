c80058eb6d0c88b4630a15cb9c7c2cc22745086b
EdvinBroman1 <47634595+EdvinBroman1@users.noreply.github.com>
2020-06-19 15:55:29 +0200

    Initial Commit

---
+++ Client.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoSio860
+{
+    class Client
+    {
+        public string ProcessStatus;
+        public static Process Tibia;
+        public static IntPtr BaseAddress;
+        public bool isAttached;
+        public Client()
+        {
+            AttachClient();
+        }
+
+
+
+        bool AttachClient()
+        {
+            List<Process> procs = Process.GetProcessesByName("Tibia").ToList();
+            if(procs.Count == 0)
+            {
+                this.ProcessStatus = "Couldn't find Tibia.exe";
+                this.isAttached = false;
+                return false;
+            }
+            else
+            {
+                this.ProcessStatus = "Currently Attached To Tibia";
+                Tibia = procs.FirstOrDefault();
+                BaseAddress = Tibia.MainModule.BaseAddress;
+                this.isAttached = true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Healer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoSio860
+{
+    class Healer
+    {
+        [DllImport("user32.dll")]
+        static extern bool PostMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
+        const uint WM_KEYDOWN = 0x0100;
+        static Memory mem = new Memory(Client.Tibia);
+        public static void SendHeal(string PlayerName)
+        {
+            string spell = "Exura Sio \"" + PlayerName +"\0"; 
+            mem.WriteStringPointer((UInt32)Client.BaseAddress + 0x24C25C, spell, new UInt32[] { 0x34, 0x40, 0x2C });
+            PostMessage(Client.Tibia.MainWindowHandle, WM_KEYDOWN, (int)Keys.Enter, 0);
+        }
+    }
+}
